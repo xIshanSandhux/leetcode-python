10075854f1a5e050744d29558020f2738c7daf4d
Ishan Sandhu <129102973+xIshanSandhux@users.noreply.github.com>
2025-03-27 13:06:47 -0400

    Solved leetcode problem 217

---
+++ leetcode_217.py
@@ -0,0 +1,41 @@
+from typing import List
+
+# Leetcode problem 217. Contains Duplicate
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        b = False
+        
+        if len(nums)==0:
+            return False
+        
+        for i in range(len(nums)):
+            num1 = nums[i]
+            for k in range(i+1, len(nums)):
+                num2 = nums[k]
+                if num1==num2:
+                    b = True
+                    return b
+        return b
+    
+    # need to use hash for it, because the lookup is O(1) cause it direcly checks if the specific value is in the hash table or not
+    # use set() cause need to store unique values
+    # and then it just looks up the value if its present or not.
+    def containsDuplicateoptimized(self, nums: List[int]) -> bool:
+        seen = set()
+
+        for i in range(len(nums)):
+            num = nums[i]
+            if num in seen:
+                return True
+            else:
+                seen.add(num)
+        return False
+
+    
+
+solution = Solution()
+
+# Test cases
+print(solution.containsDuplicateoptimized([1, 2, 3, 1]))  # True
+print(solution.containsDuplicateoptimized([1, 2, 3, 4, 5, 7]))  # False
+print(solution.containsDuplicateoptimized([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))  # True
